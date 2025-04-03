442ef6d48877d071158149724fcce2a29f2b38c0
RonStrauss <ashbelthor@gmail.com>
2025-04-03 21:20:53 +0300

    fix packages, eslint

--- src/util/deconstructors.ts
+++ src/util/deconstructors.ts
@@ -1,17 +1,17 @@
-import { Day, ValidDay, ValidHour, ValidMinute, ValidMonth } from '../types/hours';
+import { Day, Hour, ValidDay, ValidHour, ValidMinute, ValidMonth } from '../types/hours';
 
 export function getDayFromDayType(day: Day): ValidDay {
-  return Number(day.dayValue.split('/')[0]) as ValidDay;
+	return Number(day.dayValue.split('/')[0]) as ValidDay;
 }
 
 export function getMonthFromDayType(day: Day): ValidMonth {
-  return Number(day.dayValue.split('/')[1]) as ValidMonth;
+	return Number(day.dayValue.split('/')[1]) as ValidMonth;
 }
-  //@ts-ignore
-export function getHourFromHours(hourValue: Day['hours']['in'] | Day['hours']['out']): ValidHour {
-  return Number(hourValue.split(':')[0]) as ValidHour;
+
+export function getHourFromHours(hourValue: Hour): ValidHour {
+	return Number(hourValue.split(':')[0]) as ValidHour;
 }
-  //@ts-ignore
-export function getMinutesFromHours(hourValue: Day['hours']['in'] | Day['hours']['out']): ValidMinute {
-  return Number(hourValue.split(':')[1]) as ValidMinute;
+
+export function getMinutesFromHours(hourValue: Hour): ValidMinute {
+	return Number(hourValue.split(':')[1]) as ValidMinute;
 }
--- src/util/formData.ts
+++ src/util/formData.ts
@@ -1,15 +1,15 @@
 export function jsonToFormData(jsonObject: Record<string, string>): string {
-  return Object.entries(jsonObject)
-    .map(entry => entry.join('='))
-    .join('&');
+	return Object.entries(jsonObject)
+		.map((entry) => entry.join('='))
+		.join('&');
 }
 
 export function formDataToJSON(formData: string): Record<string, string> {
-  return Object.fromEntries(
-    formData.split('&').map(pair => {
-      // handle undefined values
-      const [key, value = ''] = pair.split('=');
-      return [key, value];
-    }),
-  );
+	return Object.fromEntries(
+		formData.split('&').map((pair) => {
+			// handle undefined values
+			const [key, value = ''] = pair.split('=');
+			return [key, value];
+		}),
+	);
 }
